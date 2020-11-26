cb5a57dc8c9763f9c8bbe08ada9ab72418d4d4f0
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-11-26 17:41:40 +0100

    G: Have game supplier

---
+++ TTTCore/TTTCore/GameManager.swift
@@ -0,0 +1,27 @@
+//
+//  GameManager.swift
+//  TTTCore
+//
+//  Created by 2020-DEV-174 on 26/11/2020.
+//  Copyright © 2020 2020-DEV-174. All rights reserved.
+//
+
+import Foundation
+
+
+
+/// Supplier of new games
+public struct GameManager {
+
+	public static func createGame() -> Game {
+		Game()
+	}
+
+}
+
+
+
+/// A game
+public class Game {
+
+}
