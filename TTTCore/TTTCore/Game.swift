82284be06a995e60408e414886098486e71a2190
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-11-26 18:01:58 +0100

    G: Game is authorative on rules

--- TTTCore/TTTCore/Game.swift
+++ TTTCore/TTTCore/Game.swift
@@ -13,6 +13,24 @@ import Foundation
 /// A game
 public class Game {
 
+	init() {}
+
+
+
+	/// Rules for display to user
+	///
+	/// This class encapsulates play logic and hence is the authority on the rules of play.
+	public func rules() -> String {
+		"""
+		This is a game for one or two players
+		Player 1 is X and always goes first.
+		Players cannot play on a played position.
+		Players alternate placing X’s and O’s on the board until either one player has three in \
+		a row, horizontally, vertically or diagonally, or all nine squares are filled.
+		If a player is able to draw three X’s or three O’s in a row, that player wins.
+		If all nine squares are filled and neither player has three in a row, the game is a draw.
+		"""
+	}
 }
 
 
