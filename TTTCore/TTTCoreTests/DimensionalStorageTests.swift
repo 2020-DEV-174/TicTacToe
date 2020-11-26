f83556d825d2a3924c1a890ac238c3b12e775d10
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-11-27 00:04:48 +0100

    R: Expect to have dimensional storage for game board

---
+++ TTTCore/TTTCoreTests/DimensionalStorageTests.swift
@@ -0,0 +1,35 @@
+//
+//	DimensionalStorageTests.swift
+//	TTTCore
+//
+//	Created by 2020-DEV-174 on 26/11/2020
+//	Copyright © 2020 2020-DEV-174. All rights reserved.
+//
+
+import XCTest
+import TTTCore
+
+
+
+class DimensionalStorageTests: XCTestCase {
+
+	override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+
+	func testCountReflectsDimensions() throws {
+		typealias Element = Int
+		let dimensions = [3,3]
+		let count = dimensions.reduce(1, *)
+		let initial = Element(0)
+		let storage = DimensionalStorage<Element>(dimensions, initialValue: initial)
+		XCTAssertEqual(storage.dimensions, dimensions)
+		XCTAssertEqual(storage.count, count)
+	}
+
+}
+
