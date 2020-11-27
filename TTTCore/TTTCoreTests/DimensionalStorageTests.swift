5884b4ca4905e85c40e8892be89bf5d94b45618f
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-11-27 09:24:06 +0100

    R: DimensionalStorage should have consistent position to/from index conversion

--- TTTCore/TTTCoreTests/DimensionalStorageTests.swift
+++ TTTCore/TTTCoreTests/DimensionalStorageTests.swift
@@ -31,5 +31,51 @@ class DimensionalStorageTests: XCTestCase {
 		XCTAssertEqual(storage.count, count)
 	}
 
+	func testPositionsConsistentWithIndeces() {
+		let storage = DimensionalStorage<Int>(dimensions: [3,4])
+		var indecesVisited = Set<Int>()
+		var position = [0,0]
+
+		for i in 0 ..< storage.dimensions[0] {
+			position[0] = i
+			for j in 0 ..< storage.dimensions[1] {
+				position[1] = j
+
+				let index = storage.indexOf(position: position)
+				indecesVisited.insert(index)
+				let position2 = storage.positionOf(index: index)
+
+				XCTAssert(0 <= index && index < storage.count)
+				XCTAssertEqual(position, position2)
+			}
+		}
+
+		XCTAssertEqual(storage.count, indecesVisited.count, "Duplicate indeces or incomplete traversal")
+	}
+
+	func testIndecesConsistentWithPositions() {
+
+		func test(with dimensions: [Int]) {
+			let storage = DimensionalStorage<Int>(dimensions: dimensions)
+			var positionsVisited = Set<[Int]>()
+
+			for i in 0 ..< storage.count {
+				let position = storage.positionOf(index: i)
+				positionsVisited.insert(position)
+				let index = storage.indexOf(position: position)
+
+				XCTAssertEqual(index, i)
+			}
+
+			XCTAssertEqual(storage.count, positionsVisited.count, "Duplicate positions or incomplete traversal")
+		}
+
+		test(with: [0])
+		test(with: [1])
+		test(with: [2])
+		test(with: [2,3])
+		test(with: [3,4,5])
+	}
+
 }
 
