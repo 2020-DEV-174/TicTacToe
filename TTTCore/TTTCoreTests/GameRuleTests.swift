9e1cb1a193e8acc33f209891571892052124015d
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-11-28 08:24:06 +0100

    Add Game rule tests

---
+++ TTTCore/TTTCoreTests/GameRuleTests.swift
@@ -0,0 +1,26 @@
+//
+//	GameRuleTests.swift
+//	TTTCore
+//
+//	Created by 2020-DEV-174 on 28/11/2020
+//	Copyright © 2020 2020-DEV-174. All rights reserved.
+//
+
+import XCTest
+import TTTCore
+
+
+
+class GameRuleTests: XCTestCase {
+
+	override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+
+}
+
+
