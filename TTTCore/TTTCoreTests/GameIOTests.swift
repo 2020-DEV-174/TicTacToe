f41441ea6287e5402acf1a73ef956acbc647e213
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-11-28 17:23:48 +0100

    Add I/O Tests

---
+++ TTTCore/TTTCoreTests/GameIOTests.swift
@@ -0,0 +1,24 @@
+//
+//	GameIOTests.swift
+//	TTTCore
+//
+//	Created by 2020-DEV-174 on 28/11/2020
+//	Copyright © 2020 2020-DEV-174. All rights reserved.
+//
+
+import XCTest
+import TTTCore
+
+
+
+class GameIOTests: XCTestCase {
+
+	override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+
+}
