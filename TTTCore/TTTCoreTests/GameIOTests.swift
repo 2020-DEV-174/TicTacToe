c8b19e572107362906480f5db0a9466d957d87c6
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-11-30 07:13:01 +0100

    R: I/O: game state should be observable

--- TTTCore/TTTCoreTests/GameIOTests.swift
+++ TTTCore/TTTCoreTests/GameIOTests.swift
@@ -8,10 +8,13 @@
 
 import XCTest
 import TTTCore
+import Combine
 
 
 
 class GameIOTests: XCTestCase {
+
+	var keepAlive = Array<AnyCancellable>()
 
 	override func setUpWithError() throws {
 		// Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,6 +22,29 @@ class GameIOTests: XCTestCase {
 
 	override func tearDownWithError() throws {
 		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		keepAlive.removeAll()
+	}
+
+	func testGameStateChangeIsObservable() {
+		let game = GameManager.createGame()
+		let initialState = game.state
+		var receivedState = initialState
+		game.$state
+			.sink { receivedState = $0 }
+			.store(in: &keepAlive)
+		let player1 = game.addPlayer("Player 1")
+		let player2 = game.addPlayer("Player 2")
+		var outcome = game.start()
+		XCTAssertNotNil(try? outcome.get(), "Game didn't start: \(outcome)")
+
+		//
+		XCTAssertNotEqual(receivedState.stage, initialState.stage)
+		XCTAssertEqual(receivedState.stage, game.state.stage)
+		XCTAssertEqual(receivedState.stage, .nextPlayBy(player1))
+		let position = [0,0]
+		outcome = game.play(player1, at: position)
+		XCTAssertEqual(receivedState.stage, .nextPlayBy(player2))
+		XCTAssertEqual(receivedState.board[position], player1)
 	}
 
 }
