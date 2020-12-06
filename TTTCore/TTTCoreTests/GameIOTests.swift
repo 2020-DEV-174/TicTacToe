6bff7dcd59fedb0562183838f7ea5862e46718f1
2020-DEV-174 <torsten.louland@satisfyingstructures.com>
2020-12-06 16:00:55 +0100

    R: I/O: game should allow a player host to request starting a new game

--- TTTCore/TTTCoreTests/GameIOTests.swift
+++ TTTCore/TTTCoreTests/GameIOTests.swift
@@ -104,6 +104,55 @@ class GameIOTests: XCTestCase {
 		XCTAssertEqual(game.state.board[position], game.playerNumber(withTag: tag1))
 	}
 
+	func testPlayerHostCanStartAnotherGame() {
+		let game = GameManager.createGame()
+		let subject = PassthroughSubject<Game.PlayerHostMessage, Never>()
+		let result = game.addPlayerHost(subject.eraseToAnyPublisher())
+		XCTAssertNotNil(try? result.get(), "Could not add player host because \(result)")
+		let tag1 = UUID()
+		let tag2 = UUID()
+		subject.send(.addPlayer(name: "Player 1", tag: tag1))
+		XCTAssertNotNil(game.players.first(where: { $0.tag == tag1 }))
+		subject.send(.addPlayer(name: "Player 2", tag: tag2))
+		XCTAssertNotNil(game.players.first(where: { $0.tag == tag2 }))
+		subject.send(.startGame)
+		XCTAssertEqual(game.stage, .nextPlayBy(1))
+		//
+
+		func play(moves: Int = .max) {
+			let player1starts: Bool
+			if case .nextPlayBy(1) = game.stage { player1starts = true } else { player1starts = false }
+			let t1 = player1starts ? tag1 : tag2
+			let t2 = player1starts ? tag2 : tag1
+			var move = 0
+			for (playerTag, position) in [
+				(t1, [0,0]), (t2, [2,0]),
+				(t1, [0,1]), (t2, [2,1]),
+				(t1, [2,2]), (t2, [0,2]),
+				(t1, [1,1])
+			] {
+				guard move < moves else { break }
+				move += 1
+				XCTAssertEqual(game.state.board[position], Game.noPlayerNumber)
+				subject.send(.playMove(position: position, tag: playerTag))
+				XCTAssertEqual(game.state.board[position], game.playerNumber(withTag: playerTag))
+			}
+		}
+
+		let moves = 3
+		play(moves: moves)
+		XCTAssertEqual(game.state.board.count - moves, game.state.playable.count(of: true))
+		subject.send(.restartGame)
+		XCTAssertEqual(game.stage, .nextPlayBy(1))
+		XCTAssertEqual(game.state.board.count, game.state.playable.count(of: true))
+		play()
+		XCTAssertEqual(game.stage, .wonBy(1))
+		XCTAssertEqual(0, game.state.playable.count(of: true))
+		subject.send(.startGame)
+		if case .nextPlayBy = game.stage {} else { XCTFail("Expected game at stage .nextPlayBy, but stage is \(game.stage)")}
+		XCTAssertEqual(game.state.board.count, game.state.playable.count(of: true))
+	}
+
 	// Future (not a priority at the moment): test that player host can leave game
 	// explicitly or on dealloc, and remove all added players and leave game in
 	// consistent state.
